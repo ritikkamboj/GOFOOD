19ae0cdb1b4eeb209d42f0a6b418cdde887a2caf
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2024-12-04 16:19:16 +0530

    add folder to repository

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Home from "./screens/Home";
+// import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Home />
+      </div>
+      ;
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/screens/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      <p> some thing of hoem screen </p>
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
