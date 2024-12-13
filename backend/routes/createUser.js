d0db5613aea1fe1857537ff037e7d9a2de9b5279
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2024-12-13 14:29:54 +0530

    created working login user

--- backend/routes/createUser.js
+++ backend/routes/createUser.js
@@ -38,4 +38,47 @@ router.post(
         }
     }
 );
+
+
+router.post(
+    "/loginuser",
+    body("email").isEmail(),
+    // body("password", "Password is not of required length").isLength({ min: 5 }),
+    // body("name").isLength({ min: 5 }),
+
+    async (req, res) => {
+        const error = validationResult(req);
+        if (!error.isEmpty()) {
+            return res.status(400).json({
+                success: false,
+                error: error.array(),
+            });
+        }
+        let email = req.body.email;
+
+        try {
+            let user = await User.findOne({ email });
+            if (!user) {
+                return res.status(400).json({
+                    error: 'use Correct login credentials 1 '
+                })
+            }
+
+            if (!(req.body.password === user.password)) {
+                return res.status(400).json({
+                    error: 'use Correct login credentials 2 '
+                })
+            }
+
+            res.json({
+                success: true,
+            });
+        } catch (err) {
+            console.log(err);
+            res.json({
+                success: false,
+            });
+        }
+    }
+);
 module.exports = router;
--- frontend/src/screens/Login.jsx
+++ frontend/src/screens/Login.jsx
@@ -1,7 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function Login() {
-  return <div>Login</div>;
+  let navigate = useNavigate();
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const response = await fetch("http://localhost:5000/api/loginuser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: credentials.email,
+        password: credentials.password,
+      }),
+    });
+    console.log(response);
+
+    const json = await response.json();
+    console.log(json);
+
+    if (!json.success) {
+      alert("please enter right credentials ");
+    } else {
+      navigate("/");
+    }
+  }
+
+  function onChange(event) {
+    setCredentials((prevCredentials) => ({
+      ...prevCredentials,
+      [event.target.name]: event.target.value,
+    }));
+  }
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label className="m-2" for="exampleInputEmail1">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control m-2"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+            name="email"
+            value={credentials.email}
+            onChange={onChange}
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            We'll never share your email with anyone else.
+          </small>
+        </div>
+        <div className="form-group">
+          <label className="m-2" for="exampleInputPassword1">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control m-2"
+            id="exampleInputPassword1"
+            placeholder="Password"
+            name="password"
+            value={credentials.password}
+            onChange={onChange}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-success m-2">
+          Submit
+        </button>
+        <button type="submit" className="btn btn-danger m-2">
+          <Link to="/signup" className="nav-link">
+            I am a New User
+          </Link>
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
